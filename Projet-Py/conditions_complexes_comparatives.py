090e90d85e303d25715d18b976bf7aae67984466
Antow <tonin64@live.fr>
2023-07-16 16:50:29 +0200

    Add files via upload

---
+++ Projet-Py/conditions_complexes_comparatives.py
@@ -0,0 +1,9 @@
+#Utilisation des conditions complexes avec des expressions comparatives
+nombre_sieges = 30
+nombre_divinites = 25
+
+if nombre_divinites < nombre_sieges:
+    print("autoriser plus de divinités")
+else:
+    print("ne pas autoriser plus de divinités")
+    
