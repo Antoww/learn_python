9995e3aafbee1e49f6b5215e11a9f5b94ec8697c
Antow <tonin64@live.fr>
2023-07-16 16:43:14 +0200

    Add files via upload

---
+++ Projet-Py/deroulement_prog.py
@@ -0,0 +1,9 @@
+#Découverte des conditions
+ensoleille = False
+neige = True
+if ensoleille:
+    print("on va à la plage")
+elif neige:
+    print("on fait un bonhomme de neige")
+else:
+    print("on reste à la maison")
